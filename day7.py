83cd026f2583cfafe745b5bb7cf07f1895adb272
Guillaume Alleon <guillaume.alleon@gmail.com>
2021-12-07 21:02:03 +0100

    Adding day 7

--- day7.py
+++ day7.py
@@ -1,10 +1,11 @@
+from sys import maxsize as MAX_SIZE
+from os import stat
+import re
 from argparse import ArgumentParser
+from collections import defaultdict
+from dotenv.main import load_dotenv
 
 import numpy as np
-import re
-
-import networkx as nx
-from matplotlib import pyplot as plt
 
 from common import AdventDay
 from data import Board, Graph
@@ -12,64 +13,46 @@ from data import Board, Graph
 
 class AdventDay7(AdventDay):
     def __init__(self, test: bool = False):
-        super().__init__(day=7, year=2020, test=test)
+        super().__init__(test=test)
 
     def load_input(self):
         print("Loading input...")
         with open(self.filename, "r") as f:
             lines = f.readlines()
 
-            dg = nx.DiGraph()
+            state = []
 
-            bags = set()
-            for line in lines:
-                outer, inners = line.split(" bags contain ")
+            for i in lines[0].strip().split(","):
+                state.append(int(i))
 
-                if "no other bags" in inners:
-                    pass
-                else:
-                    inner_exp = re.compile(r"(\d+) ([\w ]+) bags?[,.]")
-                    inners = inner_exp.findall(inners)
-
-                    for weight, inner in inners:
-                        bags.add(inner)
-                        dg.add_nodes_from([outer, inner])
-                        dg.add_edge(outer, inner, weight=weight)
-
-                bags.add(outer)
-
-            print(f"There are {len(bags)} bags - {dg}")
-
-            return bags, dg
+            return state
 
     def part1(self):
-        bags, dg = self.load_input()
+        input = self.load_input()
+        best_fuel = MAX_SIZE
+        for pos in range(len(input)):
+            fuel = 0
+            for x in input:
+                fuel += abs(pos - x)
 
-        rg = dg.reverse(copy=True)
+            if fuel < best_fuel:
+                best_fuel = fuel
 
-        if "test" in self.filename:
-            pos = nx.spring_layout(rg)
-            nx.draw(rg, pos, node_size=50, with_labels=True)
-            plt.savefig("day7.png")
-
-        node = "shiny gold"
-
-        possibilities = set()
-        for bag in bags:
-            if bag != node:
-                print(f"Trying from {bag} to {node}")
-
-                paths = nx.all_simple_paths(dg, source=bag, target=node)
-
-                for path in paths:
-                    possibilities.add(bag)
-
-        if "test" not in self.filename:
-            self.submit_answer(1, len(possibilities))
-        return len(possibilities)
+        return best_fuel
 
     def part2(self):
-        return 0
+        input = self.load_input()
+        best_fuel = MAX_SIZE
+        for pos in range(len(input)):
+            fuel = 0
+            for f, x in enumerate(input):
+                delta = abs(pos - x)
+                fuel += delta * (delta + 1) / 2
+
+            if fuel < best_fuel:
+                best_fuel = fuel
+
+        return best_fuel
 
 
 # define main

c7912d1ed02a55bb3585ebe3f6be0ff08bd9b857
Guillaume Alleon <guillaume.alleon@gmail.com>
2021-12-17 08:01:01 +0100

    Adding day17

--- day17.py
+++ day17.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from collections import defaultdict
 
 import numpy as np
+import math
 
 from common import AdventDay
 from data import Board, Graph
@@ -14,58 +15,53 @@ class AdventDay17(AdventDay):
 
     def load_input(self):
         print("Loading input...")
-        outputs = []
+        x_min, x_max, y_min, y_max = 0, 0, 0, 0
         with open(self.filename, "r") as f:
             lines = f.readlines()
 
-            for line in lines:
-                outputs.append(line.strip())
+            tb = lines[0][len("target area: ") :]
+            x, y = map(str.strip, tb.split(","))
 
-            return outputs
+            x_min, x_max = map(int, x[2:].split(".."))
+            y_min, y_max = map(int, y[2:].split(".."))
 
-    def process(self, values, part):
+            return x_min, x_max, y_min, y_max
 
-        board = Board.from_text(values)
-
-        n = 1
-        if part == 2:
-            n = 5
-
-        tb = Board(n * board._height, n * board._width)
-        for ib in range(n):
-            for jb in range(n):
-                for xy, value in board._kv.items():
-                    value = (int(value) + ib + jb - 1) % 9 + 1
-                    tb[xy[0] + ib * board._width, xy[1] + jb * board._height] = value
-
-        exit = (tb._width - 1, tb._height - 1)
-        print(tb._height, tb._width, exit)
-
-        hiphop = []
-        heappush(hiphop, (0, (0, 0)))
-        seen = {(0, 0)}
-        while hiphop:
-            distance, pos = heappop(hiphop)
-            if pos == exit:
-                break
-
-            x, y = pos
-            for neighbour in tb.neighbors(x, y, diagonals=False, valid_only=True):
-                if neighbour not in seen:
-                    seen.add(neighbour)
-                    heappush(hiphop, (distance + int(tb[neighbour]), neighbour))
-
-        return distance
+    def launch(self, vx, vy, x_min, x_max, y_min, y_max):
+        x = 0
+        y = 0
+        max_y = -math.inf
+        while x <= x_max and y >= y_min:
+            x += vx
+            y += vy
+            max_y = max(max_y, y)
+            vx = max(vx - 1, 0)
+            vy -= 1
+            if x_min <= x <= x_max and y_min <= y <= y_max:
+                return max_y
+        return -math.inf
 
     def part1(self):
-        lines = self.load_input()
+        x_min, x_max, y_min, y_max = self.load_input()
 
-        print(self.process(lines, 1))
+        height = 0
+        for x in range(1000):
+            for y in range(-1000, 1000):
+                height = max(height, self.launch(x, y, x_min, x_max, y_min, y_max))
+        return height
 
     def part2(self):
-        lines = self.load_input()
+        x_min, x_max, y_min, y_max = self.load_input()
 
-        print(self.process(lines, 2))
+        success = 0
+        for x in range(1000):
+            for y in range(-1000, 1000):
+                if self.launch(x, y, x_min, x_max, y_min, y_max) != -math.inf:
+                    success += 1
+
+        return success
+
+        print(b)
 
 
 # define main
